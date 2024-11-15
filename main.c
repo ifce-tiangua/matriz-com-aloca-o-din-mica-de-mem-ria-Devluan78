f293d1a910705a3d5ca91cb1bb09aaf1c66f95ea
Luan siqueira <luansiqueira765@gmail.com>
2024-11-15 15:26:01 -0300

    Resolvi o exercicio sobre vetores

--- main.c
+++ main.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
+    
+   int **mat; 
+   int linhas,colunas;
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+   scanf("%d %d",&linhas,&colunas);
+
+   if(linhas == 0){
+     printf("matriz vazia");
+   }
+  
+   mat = malloc(linhas*sizeof(int*));
+
+   for(int i=0;i<linhas;i++){
+     mat[i] = malloc(colunas * sizeof(int));
+   }
+   for(int i=0;i<linhas;i++){
+      for(int j=0;j<colunas;j++){
+           scanf("%d",&mat[i][j]);
+      }
+   }
+
+   for(int i=0;i<linhas;i++){
+      for(int j=0;j<colunas;j++){
+           printf("%d ",mat[i][j]);
+      }
+   }
+  
+   for(int i=0;i<linhas;i++){
+    free(mat[i]);
+   }
 
     return 0;
 }
